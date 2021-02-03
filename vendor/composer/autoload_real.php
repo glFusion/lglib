2b304a4b387186967db260c5058d2642286c9416
Lee Garner <lee@leegarner.com>
2021-02-03 15:31:29 -0800

    Update html-php utilities

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit12f5ae1bb871f3107ff4e608d74f8584
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
